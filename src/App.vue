eab98611c16c9e3eead9071706155c227e890602
Matteo Romagnoni <matteo.romagnoni@soccerment.com>
2026-09-10 13:28:45 +0200

    target blank

--- src/App.vue
+++ src/App.vue
@@ -66,7 +66,7 @@ const links = [
       <span class="footer-legal">{{ club.legal }}</span>
 
 
-      <a class="footer-credit" href="https://www.linkedin.com/in/matteo-romagnoni-640878235/">
+      <a class="footer-credit" href="https://www.linkedin.com/in/matteo-romagnoni-640878235/" target="_blank">
 
         © 2026 <svg class="linkedin-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"
           fill="currentColor">
