40262ac04f8c370fa69a489019ee03541dd0e455
Matteo Romagnoni <matteo.romagnoni@soccerment.com>
2026-09-22 12:25:47 +0200

    pijateli!

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,11 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   { path: '/', name: 'home', component: () => import('../views/HomeView.vue') },
   { path: '/squadra', name: 'squadra', component: () => import('../views/SquadraView.vue') },
-   { path: '/serie', name: 'serie', component: () => import('../views/SerieView.vue') },
+  { path: '/serie', name: 'serie', component: () => import('../views/SerieView.vue') },
   { path: '/serie/storia/:id', name: 'episodio', component: () => import('../views/EpisodioView.vue'), props: true },
   { path: '/serie/podcast/:id', name: 'podcast-episodio', component: () => import('../views/PodcastEpisodioView.vue'), props: true },
   { path: '/dove-siamo', name: 'dove-siamo', component: () => import('../views/DoveSiamoView.vue') },
-  { path: '/giovanili', name: 'giovanili', component: () => import('../views/GiovaniliView.vue') },
+  //{ path: '/giovanili', name: 'giovanili', component: () => import('../views/GiovaniliView.vue') },
 ]
 
 const router = createRouter({
